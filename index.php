45ccb6b208317260257eee06388965dfe322a028
Christian Scott <irchristianscott@gmail.com>
2021-07-20 12:51:04 +0300

    Only with sent param to send email

--- index.php
+++ index.php
@@ -6,11 +6,12 @@ use Postmark\PostmarkClient;
 
 $client = new PostmarkClient('c1943d94-28ce-4a84-8632-054750c2b55e');
 
-$result = $client->sendEmail(
-    "hello@zetasistemi.org",
-    "d.bentura@carolazeta.com",
-    "My test email from Postmark",
-    "Hello Mr. Benturahere,\nHere is my email sent from postmark APIs using PHP.\n\nBest regards,\nChristian Lurhakwa"
-);
-
-echo "Email Sent !";
+if(isset($_REQUEST['mail'])) {
+    $result = $client->sendEmail(
+        "hello@zetasistemi.org",
+        "d.bentura@carolazeta.com",
+        "My test email from Postmark",
+        "Hello Mr. Benturahere,\nHere is my email sent from postmark APIs using PHP.\n\nBest regards,\nChristian Lurhakwa"
+    );
+    echo "Email Sent !";
+}
